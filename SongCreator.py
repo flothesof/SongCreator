b3314a4e99de396c72ce1c24df005c177bcff3ba
Florian Le Bourdais <florian.s.lebourdais@gmail.com>
2013-09-18 09:20:32 +0200

    replaced colormap for melody

--- SongCreator.py
+++ SongCreator.py
@@ -9,7 +9,6 @@ from PyQt4 import QtGui, uic
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 import matplotlib.pyplot as plt
-import random
 import os
 import numpy as np
 
@@ -61,8 +60,8 @@ class MainWindow(QtGui.QMainWindow):
     def plot_chord_changes(self, chord_changes):
         occurences = [chord_changes[key] for key in chord_changes]
         plt.figure(self.chord_ui.figure.number)
+        plt.clf()
         ax = self.chord_ui.figure.add_subplot(111)
-        ax.hold(False)
         ax.bar(np.arange(len(occurences)), occurences)
         plt.xticks(0.5 + np.arange(len(occurences)))
         locs, labels = plt.xticks()
@@ -75,9 +74,9 @@ class MainWindow(QtGui.QMainWindow):
     def plot_melody_matrix(self, note_chord_dict):
         plt.figure(self.melody_ui.figure.number)
         plt.clf()        
-        ax = self.melody_ui.figure.add_subplot(111)
+        self.melody_ui.figure.add_subplot(111)
         note_arr = np.array([note_chord_dict[key] for key in note_chord_dict])
-        plt.matshow(note_arr, fignum=False, cmap=plt.cm.gray)
+        plt.matshow(note_arr, fignum=False, cmap=plt.cm.cool)
         plt.xticks(np.arange(0, 12), ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']);
         plt.yticks(np.arange(0, len(note_chord_dict.keys())), note_chord_dict.keys());
         plt.colorbar(shrink=0.75)
@@ -92,8 +91,8 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.lineEdit.setText(fname)
         self.mxml_extractor = MusicXML_utility.MusicXMLExtractor(fname)
         self.mxml_extractor.read_xml_from_zip()
-        if self.mxml_extractor.has_valid_xml():
-            chord_changes = self.mxml_extractor.calc_chord_changes()
+        if self.mxml_extractor.has_valid_data():
+            chord_changes = self.mxml_extractor.parse_chord_changes()
             self.plot_chord_changes(chord_changes)
             note_chord_dict = self.mxml_extractor.parse_melody_with_harmony()
             self.plot_melody_matrix(note_chord_dict)
