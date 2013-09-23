f4397bf642b31106ae57fb37bc3ac8932e40976e
Florian Le Bourdais <florian.s.lebourdais@gmail.com>
2013-09-23 11:17:37 +0200

    Added folder browser

--- MusicXML_utility.py
+++ MusicXML_utility.py
@@ -25,12 +25,62 @@ def triplet_to_string(triplet):
     else:
         return " ".join([increment_note(root, int(alter)), kind])
 
+def convert_chord_label_for_display(label):
+    label_dict = {"major":"", "minor":"m", "dominant":"7",
+                  "major-seventh":"maj7", "minor-seventh":"m7",
+                  "half-diminished":"m7b5", "augmented-seventh":"7#5"}
+    return label_dict[label]
+
+def convert_chord_label_classify(label):
+    label_dict = {"major":"", "minor":"m", "dominant":"",
+                  "major-seventh":"", "minor-seventh":"m",
+                  "half-diminished":"dim", "augmented":"+"}
+    return label_dict[label]
+
+def get_chord_from_harmony_tag(harmony_tag, transpose=0, display=True):
+    """Extracts chords from a harmony tag. When necessary, this function
+    can transpose by a given number of half-tones or also apply the labels
+    needed for classifying chords by roots"""
+    root = increment_note(harmony_tag.find('root/root-step').text, transpose)
+    alter_elem = harmony_tag.find('root/root-alter')
+    if alter_elem != None:
+        root = increment_note(root, int(alter_elem.text))
+    kind = harmony_tag.find('kind').text
+    if display:
+        kind = convert_chord_label_for_display(kind)                
+    else:
+        kind = convert_chord_label_classify(kind)
+    return "".join([root, kind])
+    
+def get_note_from_note_tag(note_tag, transpose=0):
+    """extracts note from a tag, transposing when necessary"""
+    pitch = note_tag.find("pitch")
+    if pitch != None: #in case the note is a rest
+        note = increment_note(pitch.find("step").text, transpose)
+        if pitch.find("alter") != None:
+            note = increment_note(note, int(pitch.find("alter").text))
+        return note
+    else:
+        return None
+
+def distance_between_notes(first_note, second_note):
+    """ returns distance between two notes, as a number of half-tones"""
+    notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    first_index = notes.index(first_note)
+    second_index = notes.index(second_note)
+    distance = second_index - first_index
+    if distance < 0:
+        return distance + 12
+    else:
+        return distance
+
 #==============================================================================
 # Main class
 #==============================================================================
 
 class MusicXMLExtractor(object):
     def __init__(self, filename):
+        """initializes a MusicXML extractor with a filename"""
         self.filename = str(filename)
         self.tree = None
         self.chord_changes = None
@@ -52,29 +102,21 @@ class MusicXMLExtractor(object):
                 self.tree = ET.fromstringlist(xml)
         
     def has_valid_data(self):
+        """ returns True if the parsing object created from the XML exists"""
         return self.tree != None        
     
-
-    def parse_chord_changes(self):
+    def parse_chord_changes(self, transpose=0, display=True):
         """function that returns the chord changes in the song and their 
         frequency as a dictionary"""
-        
-        def parse_chords(tree):
-            """function that parses a MusicXML and returns a list of chords"""
-            chords = []
-            for elem in tree.findall('.//harmony'):
-                root = elem.find('root/root-step').text
-                alter_elem = elem.find('root/root-alter')
-                alter = (alter_elem != None and alter_elem.text)
-                if 'text' in elem.find('kind').attrib:
-                    kind_attrib = elem.find('kind').attrib['text']
-                    chords.append((root, alter, kind_attrib))
-                else:
-                    kind = elem.find('kind').text
-                    chords.append((root, alter, kind))
-            return map(triplet_to_string, chords)
+        chords = []        
+        for measure in self.tree.findall(".//measure"):
+            for child in measure:
+                if child.tag == 'harmony':
+                    chords.append(
+                        get_chord_from_harmony_tag(child, 
+                                                   transpose=transpose,
+                                                   display=display))
             
-        chords = parse_chords(self.tree)
         chord_changes = {}
         for ind, val in enumerate(chords[:-1]):
             transition = "->".join([val, chords[ind + 1]])
@@ -86,33 +128,15 @@ class MusicXMLExtractor(object):
         return chord_changes
         
         
-    def parse_melody_with_harmony(self):
-        def get_chord_from_harmony_tag(harmony_tag):
-            root = harmony_tag.find('root/root-step').text
-            alter_elem = harmony_tag.find('root/root-alter')
-            if alter_elem != None:
-                root = increment_note(root, int(alter_elem.text))
-            if 'text' in harmony_tag.find('kind').attrib:
-                kind_attrib = harmony_tag.find('kind').attrib['text']
-                return " ".join([root, kind_attrib])
-            else:
-                kind = harmony_tag.find('kind').text                
-                return " ".join([root, kind])
-            
-        def get_note_from_note_tag(note_tag):
-            pitch = note_tag.find("pitch")
-            if pitch != None: #in case the note is a rest
-                note = pitch.find("step").text
-                if pitch.find("alter") != None:
-                    note = increment_note(note, int(pitch.find("alter").text))
-                return note
-            else:
-                return None
+    def parse_melody_with_harmony(self, transpose=0, display=True):
+        """returns a dictionary containing note frequencies over 
+        the chords appearing in the song"""
         
         def write_note_to_dict(note, current_chord, note_chord_dict):
             if not current_chord in note_chord_dict:
                 note_chord_dict[current_chord] = [0] * 12
-            ind = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'].index(note)
+            ind = ['C', 'C#', 'D', 'D#', 'E', 'F',
+                               'F#', 'G', 'G#', 'A', 'A#', 'B'].index(note)
             note_chord_dict[current_chord][ind] += 1
             
         current_chord = None
@@ -120,37 +144,40 @@ class MusicXMLExtractor(object):
         for measure in self.tree.findall(".//measure"):
             for child in measure:
                 if child.tag == 'harmony':
-                    current_chord = get_chord_from_harmony_tag(child)
+                    current_chord = get_chord_from_harmony_tag(child, 
+                                                   transpose=transpose,
+                                                   display=display)
                 if child.tag == 'note':
-                    note = get_note_from_note_tag(child)
+                    note = get_note_from_note_tag(child, transpose=transpose)
                     if current_chord != None and note != None:
                         write_note_to_dict(note, current_chord, note_chord_dict)
         self.note_chord_dict = note_chord_dict
         return note_chord_dict
 
-    def guess_song_key(self):
-        """returns the key guessed from the song data
-        Examples: A m or C"""
+    def parse_song_key(self):
+        """returns the major key found in the song data using the rule
+        of fifths"""
         for key in self.tree.findall(".//part/measure/attributes/key"):
             fifths = int(key.find("fifths").text)
-            mode = key.find("mode").text
         root = 'C'
         sign = int(fifths>=0 or -1)
         for i in range(abs(fifths)):
             root = increment_note(root, 7 * sign)
-        return (root, mode)
+        return root
     
 def test_function1():
     """tests the parse_chord_changes function"""
 #    filename = os.path.join(os.getcwd(), r"MusicXML_files/Keith Richards, Mick Jagger - Angie.mxl")
-    filename = os.path.join(os.getcwd(), r"MusicXML_files/Antonio Carlos Jobim - The Girl From Ipanema.mxl")
+    filename = os.path.join(os.getcwd(), 
+            r"MusicXML_files/Antonio Carlos Jobim - The Girl From Ipanema.mxl")
     m = MusicXMLExtractor(filename)
     m.read_xml_from_zip()
     print m.parse_chord_changes()
     
 def test_function2():
     """tests the parse_melody_with_harmony function"""
-    filename = os.path.join(os.getcwd(), r"MusicXML_files/Antonio Carlos Jobim - The Girl From Ipanema.mxl")
+    filename = os.path.join(os.getcwd(), 
+            r"MusicXML_files/Antonio Carlos Jobim - The Girl From Ipanema.mxl")
     m = MusicXMLExtractor(filename)
     m.read_xml_from_zip()
     print m.parse_melody_with_harmony()
@@ -162,18 +189,57 @@ def test_function3():
     for filename in filter(lambda s: s.endswith(".mxl"), filenames):        
         m = MusicXMLExtractor(os.path.join(basepath, filename))
         m.read_xml_from_zip()
-        root, mode = m.guess_song_key()
+        root = m.parse_song_key()
         note_to_watch = increment_note(root, -3 + -1)
         note_chord_dict = m.parse_melody_with_harmony()
         note_arr = np.array([note_chord_dict[key] for key in note_chord_dict])
         total_notes = note_arr.sum()
-        ind = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'].index(note_to_watch)
+        ind = ['C', 'C#', 'D', 'D#', 'E', 'F',
+                       'F#', 'G', 'G#', 'A', 'A#', 'B'].index(note_to_watch)
         note_to_watch_count = note_arr[:, ind].sum()
-        print filename, root, mode, note_to_watch_count, total_notes, 
+        print filename, root, note_to_watch_count, total_notes, 
         print "%.2f" % (note_to_watch_count / float(total_notes) * 100)
     
+def test_function4():
+    # initialize global chord index
+    all_chords = []
+    chromatic_scale = ['C', 'C#', 'D', 'D#', 'E', 'F',
+                               'F#', 'G', 'G#', 'A', 'A#', 'B']
+    for note in chromatic_scale:
+        for triad in ['', 'm', '+', 'dim', 'sus']:
+            all_chords.append("".join([note, triad]))
+    
+    # initialize chord transition matrix        
+    chord_transition_matrix = np.zeros((60, 60))
+    # initialize melody observation matrix
+    melody_observation_matrix = np.zeros((60, 12))
+    
+    basepath = os.path.join(os.getcwd(), "MusicXML_files")
+    filenames = os.listdir(basepath)
+    # loop over files in folder
+    for filename in filter(lambda s: s.endswith(".mxl"), filenames):        
+        print filename
+        m = MusicXMLExtractor(os.path.join(
+                                                    basepath, filename))
+        m.read_xml_from_zip()
+        root = m.parse_song_key()
+        distance_to_C_scale = distance_between_notes(
+                                                                root, 'C')
+        chord_changes = m.parse_chord_changes(transpose=distance_to_C_scale,
+                              display=False)
+        for item in chord_changes.keys():
+            chord1, chord2 = item.split("->")
+            chord_transition_matrix[all_chords.index(chord1), 
+                                    all_chords.index(chord2)] += chord_changes[item]
+                                    
+        note_chord_dict = m.parse_melody_with_harmony(transpose=distance_to_C_scale,
+                                                              display=False)        
+    # do some plotting
+    return chord_transition_matrix
+        
 if __name__ == "__main__":
 #    test_function1()                        
 #    test_function2()                        
-    test_function3()                        
+#    test_function3()                        
+    ctm = test_function4()                        
     
--- SongCreator.py
+++ SongCreator.py
@@ -50,12 +50,20 @@ class MainWindow(QtGui.QMainWindow):
         layout.addWidget(self.melody_ui.toolbar)
         layout.addWidget(self.melody_ui.canvas)
         self.ui.tab_2.setLayout(layout)
+
+        # folder analysis tab
+        self.folder_ui = MplFigure(self)
+        layout = self.ui.tab_3.layout()
+        layout.addWidget(self.folder_ui.toolbar)        
+        layout.addWidget(self.folder_ui.canvas)
         
     def initData(self):
         pass
         
     def connectSlots(self):
         self.ui.pushButton.clicked.connect(self.openMusicXMLFile)
+        self.ui.pushButton_2.clicked.connect(self.analyzeFolder)
+        self.ui.checkBox.toggled.connect(self.toggle_transposition)        
         
     def plot_chord_changes(self, chord_changes):
         sorted_occurences = [(key, chord_changes[key]) for key in sorted(
@@ -70,9 +78,7 @@ class MainWindow(QtGui.QMainWindow):
         locs, labels = plt.xticks()
         plt.xticks(locs, new_labels, rotation = 90)
         plt.tight_layout()
-        
-        # refresh canvas
-        self.chord_ui.canvas.draw()
+        self.chord_ui.canvas.draw() # refresh canvas
 
     def plot_melody_matrix(self, note_chord_dict):
         plt.figure(self.melody_ui.figure.number)
@@ -84,9 +90,27 @@ class MainWindow(QtGui.QMainWindow):
         plt.yticks(np.arange(0, len(note_chord_dict.keys())), note_chord_dict.keys());
         plt.colorbar(shrink=0.75)
         plt.tight_layout()
-        
-        # refresh canvas
-        self.melody_ui.canvas.draw()
+        self.melody_ui.canvas.draw() # refresh canvas
+
+    def toggle_transposition(self, toggle):
+        try:
+            valid_data = self.mxml_extractor.has_valid_data()
+        except AttributeError:
+            return
+        if valid_data:
+            if toggle:
+                root = self.mxml_extractor.parse_song_key()
+                transpose_distance = MusicXML_utility.distance_between_notes(
+                                                                    root,
+                                                                    'C')
+            else:
+                transpose_distance = 0
+            chord_changes = self.mxml_extractor.parse_chord_changes(
+                                                transpose=transpose_distance)
+            self.plot_chord_changes(chord_changes)
+            note_chord_dict = self.mxml_extractor.parse_melody_with_harmony(
+                                                transpose=transpose_distance)
+            self.plot_melody_matrix(note_chord_dict)
 
     def openMusicXMLFile(self):
         fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
@@ -95,12 +119,102 @@ class MainWindow(QtGui.QMainWindow):
         self.mxml_extractor = MusicXML_utility.MusicXMLExtractor(fname)
         self.mxml_extractor.read_xml_from_zip()
         if self.mxml_extractor.has_valid_data():
+            # GUI modification
+            self.ui.checkBox.setChecked(False)
+            root = self.mxml_extractor.parse_song_key()
+            minor_root = MusicXML_utility.increment_note(root, -3) + "m"
+            self.ui.lineEdit_2.setText(" / ".join([root, minor_root]))
+            # song parsing
             chord_changes = self.mxml_extractor.parse_chord_changes()
             self.plot_chord_changes(chord_changes)
             note_chord_dict = self.mxml_extractor.parse_melody_with_harmony()
             self.plot_melody_matrix(note_chord_dict)
 
+    def analyzeFolder(self):
+        folder_name = QtGui.QFileDialog.getExistingDirectory(self, 
+             'Select directory', os.getcwd(), QtGui.QFileDialog.ShowDirsOnly)
+             
+        filenames = os.listdir(folder_name)
+        
+        # initialize global chord index
+        all_chords = []
+        chromatic_scale = ['C', 'C#', 'D', 'D#', 'E', 'F',
+                                   'F#', 'G', 'G#', 'A', 'A#', 'B']
+        for note in chromatic_scale:
+            for triad in ['', 'm', '+', 'dim', 'sus']:
+                all_chords.append("".join([note, triad]))
+        
+        # initialize chord transition matrix        
+        chord_transition_matrix = np.zeros((60, 60))
+        # initialize melody observation matrix
+        melody_observation_matrix = np.zeros((60, 12))
 
+        # loop over files in folder
+        for filename in filter(lambda s: s.endswith(".mxl"), filenames):        
+            print filename
+            m = MusicXML_utility.MusicXMLExtractor(
+                                    os.path.join(folder_name, filename))
+            m.read_xml_from_zip()
+            root = m.parse_song_key()
+            distance_to_C_scale = MusicXML_utility.distance_between_notes(
+                                                                    root,
+                                                                    'C')
+            # parse chord transitions                                                         
+            chord_changes = m.parse_chord_changes(
+                                                transpose=distance_to_C_scale,
+                                                display=False)
+            temporary_chord_changes = np.zeros((60, 60))                                    
+            for item in chord_changes.keys():
+                chord1, chord2 = item.split("->")
+                change = chord_changes[item] 
+                temporary_chord_changes[all_chords.index(chord1), 
+                                        all_chords.index(chord2)] += change
+            
+                        
+            s = temporary_chord_changes.sum(axis=1)
+            indices = (s != 0)
+            temporary_chord_changes[indices, :] = (temporary_chord_changes[indices, :].T / s[indices]).T
+            chord_transition_matrix += temporary_chord_changes
+            
+            # parse melody                 
+            note_chord_dict = m.parse_melody_with_harmony(
+                                                transpose=distance_to_C_scale,
+                                                display=False)        
+            for chord in note_chord_dict.keys():
+                row = np.array(note_chord_dict[chord], dtype=np.float32)
+                if row.sum() != 0:
+                    row /= row.sum()
+                melody_observation_matrix[all_chords.index(chord), :] += row
+
+        # normalize chord transition matrix so each row sums to 1
+        s = chord_transition_matrix.sum(axis=1)
+        indices = (s != 0)
+        chord_transition_matrix[indices, :] = (chord_transition_matrix[indices, :].T / s[indices]).T
+        
+        # plot chord transition matrix
+        plt.figure(self.folder_ui.figure.number)
+        plt.clf()        
+        self.folder_ui.figure.add_subplot(121)
+        plt.matshow(chord_transition_matrix, fignum=False, cmap=plt.cm.cool)
+        plt.colorbar(shrink=0.75)
+        plt.xticks(5 * np.arange(12), chromatic_scale)        
+        plt.yticks(5 * np.arange(12), chromatic_scale)        
+        plt.grid(True)
+        plt.title("Chord transition matrix")
+        
+        # plot melody observation matrix
+        self.folder_ui.figure.add_subplot(122)
+        plt.matshow(melody_observation_matrix, fignum=False, 
+                    cmap=plt.cm.cool, aspect=0.2)
+        plt.colorbar(shrink=0.75)
+        plt.xticks(np.arange(12), chromatic_scale)        
+        plt.yticks(5 * np.arange(12), chromatic_scale)        
+        plt.title("Melody observation matrix")
+        
+        # refresh window        
+        plt.tight_layout()
+        self.folder_ui.canvas.draw() # refresh canvas
+                                        
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
